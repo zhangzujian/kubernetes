7c0b3b69d87c5145dc1f695c2523f643aca56054
张祖建 <zhangzujian.7@gmail.com>
2023-02-12 12:24:08 +0800

    debug

--- test/e2e/network/service.go
+++ test/e2e/network/service.go
@@ -1479,6 +1479,8 @@ var _ = common.SIGDescribe("Services", func() {
 			output, err = exec.Command("sh", "-c", cmd).CombinedOutput()
 			framework.ExpectNoError(err)
 			framework.Logf("%s", output)
+
+			time.Sleep(time.Hour)
 		}
 		framework.ExpectNoError(err)
 	})
@@ -1525,10 +1527,17 @@ var _ = common.SIGDescribe("Services", func() {
 			framework.ExpectNoError(err)
 			framework.Logf("%s", output)
 
+			cmd = "kubectl ko sbctl lflow-list ovn-default"
+			output, err = exec.Command("sh", "-c", cmd).CombinedOutput()
+			framework.ExpectNoError(err)
+			framework.Logf("%s", output)
+
 			cmd = fmt.Sprintf("kubectl ko trace %s/%s %s tcp %d", execPod.Namespace, execPod.Name, nodePortService.Spec.ClusterIP, 80)
 			output, err = exec.Command("sh", "-c", cmd).CombinedOutput()
 			framework.ExpectNoError(err)
 			framework.Logf("%s", output)
+
+			time.Sleep(time.Hour)
 		}
 		framework.ExpectNoError(err)
 	})
@@ -1578,10 +1587,16 @@ var _ = common.SIGDescribe("Services", func() {
 			framework.ExpectNoError(err)
 			framework.Logf("%s", output)
 
+			cmd = "kubectl ko sbctl lflow-list ovn-default"
+			output, err = exec.Command("sh", "-c", cmd).CombinedOutput()
+			framework.ExpectNoError(err)
+			framework.Logf("%s", output)
+
 			// cmd = fmt.Sprintf("kubectl ko trace %s/%s %s tcp %d", execPod.Namespace, execPod.Name, externalNameService.Spec.ClusterIP, 80)
 			// output, err = exec.Command("sh", "-c", cmd).CombinedOutput()
 			// framework.ExpectNoError(err)
 			// framework.Logf("%s", output)
+			// time.Sleep(time.Hour)
 		}
 		framework.ExpectNoError(err)
 	})
@@ -1633,10 +1648,16 @@ var _ = common.SIGDescribe("Services", func() {
 			framework.ExpectNoError(err)
 			framework.Logf("%s", output)
 
+			cmd = "kubectl ko sbctl lflow-list ovn-default"
+			output, err = exec.Command("sh", "-c", cmd).CombinedOutput()
+			framework.ExpectNoError(err)
+			framework.Logf("%s", output)
+
 			// cmd = fmt.Sprintf("kubectl ko trace %s/%s %s tcp %d", execPod.Namespace, execPod.Name, externalNameService.Spec.ClusterIP, 80)
 			// output, err = exec.Command("sh", "-c", cmd).CombinedOutput()
 			// framework.ExpectNoError(err)
 			// framework.Logf("%s", output)
+			// time.Sleep(time.Hour)
 		}
 		framework.ExpectNoError(err)
 	})
